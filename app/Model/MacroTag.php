3b0f7083bc3a41d29f7a3c3e56b6fc37e28cd9fd
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-02-27 21:08:42 -0500

    Update and rename app/Model to app/Model/MacroTag.php

---
+++ app/Model/MacroTag.php
@@ -0,0 +1,46 @@
+<?php 
+class MacroTag extends AppModel{
+
+	public var $hasAndBelongsToMany = array(
+		'MicroTag' => array(
+			'className' => 'MicroTag',
+			'joinTable' => 'communities',
+			'with' => 'Community',
+			'foreignKey' => 'macro_tag_id',
+			'associationForeignKey' => 'micro_tag_id'
+		),
+		'Project' => array(
+			'className' => 'Project',
+			'joinTable' => 'projects_macro_tags',
+			'with' => 'ProjectsMacroTag',
+			'foreignKey' => 'macro_tag_id',
+			'associationForeignKey' => 'project_id'
+		),
+		'Profile' => array(
+			'className' => 'Profile',
+			'joinTable' => 'profiles_macro_tags',
+			'with' => 'ProfilesMacroTag',
+			'foreignKey' => 'macro_tag_id',
+			'associationForeignKey' => 'profile_id'
+		)
+	);
+	
+	public var $validate = array(
+		'name' => array(
+			'alphaNumeric' => array
+			(
+				'rule' => 'alphaNumeric',
+				'required' => true,
+				'allowEmpty' => false,
+				'message' => 'Tag names may only contain alphanumeric characters.'
+			),
+			'between' => array
+			(
+				'rule' => array('lengthBetween', 1, 50),
+				'message' => 'Tag names must be between 1 to 75 characters.'
+			)
+			
+		)
+	);
+}
+?>
