01abe8394862f36e0d86ad50eaa3c7728670f292
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-02-28 19:10:15 -0500

    Update Tag.php

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -18,5 +18,32 @@ public class Tag extends AppModel{
 			
 		)
 	);
+	
+	/* Searches though tags for tags given a certain tag name. */
+	public function search($tag_name){
+		var $ans = find('all', 
+			array(
+				'conditions' => array('name' => $tag_name),
+				'fields' => array('id', 'name'),
+				'callbacks' => false
+			)
+		);
+		return $ans
+	}
+
+	/* Searches though tags for tags given a certain tag name.
+	- Uses LIKE comparison
+	 */
+	public function searchLike($tag_name){
+		var $condition = '%' + $tag_name + '%';
+		var $ans = find('all', 
+			array(
+				'conditions' => array('name LIKE' => $condition),
+				'fields' => array('id', 'name'),
+				'callbacks' => false
+			)
+		);
+		return $ans
+	}
 }
 ?>
