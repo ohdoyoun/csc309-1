750ff704c7a75220fd130782d290632f6f8b68a5
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-03-02 22:58:38 -0500

    Update ProjectsMicroTag.php

--- app/Model/ProjectsMicroTag.php
+++ app/Model/ProjectsMicroTag.php
@@ -2,9 +2,9 @@
 App::uses('AppModel', 'Model');
 class ProjectsMicroTag extends AppModel{
 
-	public var $useTable = 'project_micro_tags';
+	public $useTable = 'project_micro_tags';
 	
-	public var $name = 'ProjectsMicroTag';
+	public $name = 'ProjectsMicroTag';
 }
 
 ?>
