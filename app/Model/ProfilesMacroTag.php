cfeb92bb679ca8b5f87b33263d2f17605ea26544
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-03-12 20:55:54 -0400

    Update ProfilesMacroTag.php

--- app/Model/ProfilesMacroTag.php
+++ app/Model/ProfilesMacroTag.php
@@ -2,8 +2,10 @@
 App::uses('AppModel', 'Model');
 class ProfilesMacroTag extends AppModel{
 
+	// The table that this Model uses.
 	public $useTable = 'profile_macro_tags';
 	
+	// The name of this Model.
 	public $name = 'ProfilesMacroTag';
 }
 
