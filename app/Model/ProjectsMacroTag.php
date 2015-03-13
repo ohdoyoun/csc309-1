f416a0c45bf619c2df6f8eb01d39935eddac40fc
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-03-12 21:00:03 -0400

    Update ProjectsMacroTag.php

--- app/Model/ProjectsMacroTag.php
+++ app/Model/ProjectsMacroTag.php
@@ -2,8 +2,10 @@
 App::uses('AppModel', 'Model');
 class ProjectsMacroTag extends AppModel{
 
+	// The table that this Model uses.
 	public $useTable = 'project_macro_tags';
 	
+	// The name of this Model.
 	public $name = 'ProjectsMacroTag';
 }
 
