d1a4b43b12d354849e0a0860260308eb5a823984
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-03-12 20:54:42 -0400

    Update Profile.php

--- app/Model/Profile.php
+++ app/Model/Profile.php
@@ -2,11 +2,13 @@
 App::uses('AppModel', 'Model');
 class Profile extends AppModel{
 	
+	// The name of this Model.
 	public $name = 'Profile';
 	
+	// The table that this Model is linked to.
 	public $useTable = 'profiles';
 
-	/* The associations of this Model with other Models based on the database. */
+	// The association to the User Model.
 	public $belongsTo = array(
 		'User' => array(
 			'className' => 'User',
@@ -14,7 +16,9 @@ class Profile extends AppModel{
 		)
 	);
 
-	/* Associations with the Project Model. */
+	/* The associations that linked this Model to other Models.
+	- Linked to MacroTag and MicroTag models.
+	*/
 	public $hasAndBelongsToMany = array(
 		'MacroTag' => array(
 			'className' => 'MacroTag',
@@ -32,15 +36,13 @@ class Profile extends AppModel{
 		)
 	);
 
-	/* The validation rules that this model must follow. 
-	- Mostly dervived from the database.
-	*/
+	// The rules this model uses to validate.
  	public $validate = array(
 		'first_name' => array
 		(	
 			'alphaNumeric' => array
 			(
-				'rule' => array('custom', '/^[a-z0-9 ]*$/i'),
+				'rule' => 'alphaNumeric',
 				'required' => true,
 				'message' => 'Your first name may only be alphanumeric characters.',
                 'allowEmpty' => true
@@ -187,51 +189,7 @@ class Profile extends AppModel{
 				'message' => 'Biography must be between 0 to 2000 characters.'
 			)
 		),
-		'old_password' => array
-		(
-	        'rule' => 'checkCurrentPassword',
-	        'message' => 'Wrong password.',
-	        'required' => true
-	    ),
-		'new_password' => array
-		(
-			'between' => array(
-				'rule' => array('lengthBetween', 5, 16),
-				'message' => 'Your password must be between 5 and 16 characters long.',
-				'required' => true
-			),
-			'matchPassword' => array(
-				'rule' => 'matchPasswords',
-				'message' => 'Your passwords do not match!',
-				'required' => true
-			)
-		),
-		'confirm_password' => array
-		(
-			'between' => array(
-				'rule' => array('lengthBetween', 5, 16),
-				'message' => 'Your password must be between 5 and 16 characters long.',
-				'required' => true
-			)
-		)
 	);
-	
-	public function checkCurrentPassword($data) {
-	    $this->id = AuthComponent::user('id');
-	    $password = $this->field('password');
-	    return (AuthComponent::password($data['Profile']['current_password']) == $password);
-	}
-	
-	/* Checks if the password and password2 field contain the same password.
-	 - Used for the match passwords validation check. 
-	*/
-    public function matchPasswords($data) {
-        if ($data['new_password'] == $this->data['Profile']['confirm_password']) {
-            return true;
-        }
-        $this->invalidate('confirm_password', 'Your passwords do not match.');
-        return false;
-    }
 
 }
 ?>
