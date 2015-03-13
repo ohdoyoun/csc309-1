18009ec1fc3fb19811b742129b1474a15c572a70
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-03-12 20:11:15 -0400

    Update Initiator.php

--- app/Model/Initiator.php
+++ app/Model/Initiator.php
@@ -2,11 +2,15 @@
 App::uses('AppModel', 'Model');
 class Initiator extends AppModel{
 	
+	// The name of the Model.
 	public $name = 'Initiator';
 	
+	// The table that is linked with his Model.
 	public $useTable = 'initiators';
 	
 	/*
+	Add function for this table. 
+	Note - No longer needed sine there is a global add function.
 	public function addInitiator($data){
 		if(!empty($data)){
 			$this->Initiator->create();
