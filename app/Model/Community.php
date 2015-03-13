51de70f61231b2f85b0d06e590c5533b58d3cabc
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-03-12 20:08:40 -0400

    Update Community.php

--- app/Model/Community.php
+++ app/Model/Community.php
@@ -2,7 +2,10 @@
 App::uses('AppModel', 'Model');
 class Community extends AppModel{
 	
+	// The table to use.
 	public $useTable = 'communties';
+	
+	// The name of the Model.
 	public $name = 'Community';
 	
 }
