34f83a457dfd003a49ab21f5342fac2dd0a6779c
Laurkan Rodriguez <laurkan.rodriguez@gmail.com>
2015-04-03 19:10:55 -0400

    Update CommunityController.php

--- app/Controller/CommunityController.php
+++ app/Controller/CommunityController.php
@@ -1,5 +1,12 @@
 <?php
 class ControllerController extends AppController {
+  
+  public $name = 'Community';
+  
+  function index(){
+    $this->set('communities', $name->find('all'));
+  }
+  
 
 }
 ?>
